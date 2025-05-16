8b60b998c6c0e779a5b67f1812d4f42f79be1d6a
Wowowo <ryo.htc4405@gmail.com>
2025-05-17 06:36:47 +0900

    modify

--- src/graphot/__init__.py
+++ src/graphot/__init__.py
@@ -1,4 +1,4 @@
-from .solver import solve_primal, solve_dual
+from .solver import solver_primal_ot, solver_dual_ot
 from .visualize import plot_graph_with_transport, plot_graph_with_potentials
 from adj_to_dist import adj_to_dist
 from .graph_generator import gen_complete_adj_matrix, gen_cycle_adj_matrix, gen_path_adj_matrix, gen_random_adj_matrix
